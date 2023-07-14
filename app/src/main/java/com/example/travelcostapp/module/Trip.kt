97480094fcc235dd0529294e07226153e45a95e0
juliaguenther <julia_guenther@t-online.de>
2023-07-14 14:58:41 +0200

    fix db connection and display error, travelers inputs added part2

--- app/src/main/java/com/example/travelcostapp/module/Trip.kt
+++ app/src/main/java/com/example/travelcostapp/module/Trip.kt
@@ -1,9 +1,10 @@
 package com.example.travelcostapp.module
 
 data class Trip(
-    val name: String,
-    val destination: String,
-    val days: Int,
-    val amountOfTravelers: Int,
-    val travelers: List<Travelers>
+    val name: String = "",
+    val destination: String = "",
+    val days: Int = 0,
+    val amountOfTravelers: Int = 0,
+    val travelers: List<Traveler> = emptyList()
 )
+
